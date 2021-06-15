bc5ac633068034c502789e006cb11895d646640b
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-15 21:17:26 +0000

    Counter turns red when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   // --- our code goes here ---
   console.log('herpherpherrp');
-  let maxCount = 140;
+  let maxCount = 14;
   const characterCount = $('.text-area');
 
   $('.text-area').on('keypress', function() {
     const countRemaining = maxCount - $(this).val().length;
-    console.log(countRemaining);
-
-    $(this).parent().children('.counter').text(countRemaining);
-    console.log($(this))
-    console.log($(this).closest('.new-tweet').find('.counter').text(countRemaining))
+ 
+    console.log($(this).closest('.new-tweet').find('.counter').text(countRemaining));
+    if (countRemaining < 0) {
+      $(this).closest('.new-tweet').find('.counter').addClass('tooManyWords')
+    }
   })
 
 });
