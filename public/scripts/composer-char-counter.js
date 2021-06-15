385dc1a30ef4d762576c0b0a976feb5ca3c9674e
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-06-15 21:01:30 +0000

    Numbers on counter go down as you type

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log('herpherpherrp')
+  console.log('herpherpherrp');
+  let maxCount = 140;
   const characterCount = $('.text-area');
 
   $('.text-area').on('keypress', function() {
-    console.log(this);
-    console.log($(this).val().length);
-    console.log(140 - $(this).val().length);
+    const countRemaining = maxCount - $(this).val().length;
+    console.log(countRemaining);
 
+    $(this).parent().children('.counter').text(countRemaining);
+    console.log($(this))
+    console.log($(this).closest('.new-tweet').find('.counter').text(countRemaining))
   })
 
 });
